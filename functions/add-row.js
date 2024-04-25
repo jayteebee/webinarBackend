5713ebe47f721779326723ec297468edc5ccf424
Jethro Block <jethroblock22@gmail.com>
2024-04-25 15:05:05 +0100

    Add ical-generator dependency

--- functions/add-row.js
+++ functions/add-row.js
@@ -1,4 +1,125 @@
+// const nodemailer = require('nodemailer');
+// const ical = require('ical-generator');
+
+
+// // Configure nodemailer transport using environment variables
+// const transporter = nodemailer.createTransport({
+//   service: 'Gmail',
+//   auth: {
+//     user: process.env.EMAIL || "jethro@thermalvisionresearch.co.uk",
+//     pass: process.env.PASSWORD || "ThermalVR2k4",
+//   },
+// });
+
+// // Netlify function handler
+// exports.handler = async (event) => {
+//   // Set CORS headers
+//   const headers = {
+//     'Access-Control-Allow-Origin': 'http://localhost:8888',
+//     'Access-Control-Allow-Headers': 'Content-Type',
+//     'Access-Control-Allow-Methods': 'POST, OPTIONS',
+//     'Content-Type': 'application/json'
+//   };
+
+//   // Handle preflight requests (HTTP OPTIONS method)
+//   if (event.httpMethod === 'OPTIONS') {
+//     // Return a 200 status for preflight checks
+//     return {
+//       statusCode: 200,
+//       headers,
+//       body: JSON.stringify({ message: 'CORS preflight checks passed' }),
+//     };
+//   }
+
+//   // Handle POST requests
+//   if (event.httpMethod === 'POST') {
+//     const data = JSON.parse(event.body);
+//     const { firstName, email, submittedAt } = data;
+
+//     // Correct initialization of calendar
+//     const calendar = ical({name: 'Webinar Series'});  // This line is correctly calling the constructor
+
+//     const startDate = new Date('2024-04-26T09:00:00Z');
+//     const endDate = new Date(startDate.getTime() + 1800000);  // 30 minutes after start
+
+//     // Create event
+//     calendar.createEvent({
+//       start: startDate,
+//       end: endDate,
+//       summary: 'Webinar Registration',
+//       description: 'Thank you for registering to our webinar series.',
+//       organizer: 'jethro@thermalvisionresearch.co.uk',
+//       attendees: [{email: email, name: firstName}]
+//     });
+
+//     // Generate the .ics file content
+//     const icsString = calendar.toString();
+
+//     // Mail options including the .ics file attachment
+//     const mailOptionsUser = {
+//       from: process.env.EMAIL,
+//       to: email,
+//       subject: 'Thank you for registering to our Webinar Series',
+//       html: `
+//         <p>Hello ${firstName},</p>
+//         <p>Thank you for registering to our webinar series. Your calendar invitation should be with you shortly.</p>
+//         <p>Kind Regards,<br>Jethro Block<br>Business Development Manager<br>07948 725 229<br>www.thermalvisionecology.co.uk<br>2530 The Quadrant, Aztec West, Bristol BS32 4AQ</p>
+//         <img src="https://i.ibb.co/092hq5D/tvrLOGO.png" alt="tvrLOGO" border="0" height="70px">
+//       `,
+//       attachments: [{
+//         filename: 'event.ics',
+//         content: icsString,
+//         contentType: 'text/calendar'
+//       }]
+//     };
+
+//       // Send thank-you email to the user
+//       await new Promise((resolve, reject) => {
+//         transporter.sendMail(mailOptionsUser, (error, info) => {
+//           if (error) reject(error);
+//           else resolve(info);
+//         });
+//       });
+
+
+//  // Additional POST request to Zapier
+//  const zapierResponse = await fetch('https://hooks.zapier.com/hooks/catch/18365503/37hs1dq/', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(data)
+// });
+
+// if (!zapierResponse.ok) throw new Error('Failed to send data to Zapier');
+
+
+//       // Return success response with CORS headers
+//       return {
+//         statusCode: 200,
+//         headers,
+//         body: JSON.stringify({ message: 'Added row to spreadsheet' }),
+//       };
+//     } catch (error) {
+//       // Catch any errors and return a 500 error response with CORS headers
+//       return {
+//         statusCode: 500,
+//         headers,
+//         body: JSON.stringify({ error: error.message }),
+//       };
+//     }
+//   }
+
+//   // Respond to any other HTTP methods with a Method Not Allowed status
+//   return {
+//     statusCode: 405,
+//     headers,
+//     body: JSON.stringify({ message: 'Method Not Allowed' }),
+//   };
+// };
+
 const nodemailer = require('nodemailer');
+const { ICalCalendar } = require('ical-generator');
 
 // Configure nodemailer transport using environment variables
 const transporter = nodemailer.createTransport({
@@ -11,7 +132,6 @@ const transporter = nodemailer.createTransport({
 
 // Netlify function handler
 exports.handler = async (event) => {
-  // Set CORS headers
   const headers = {
     'Access-Control-Allow-Origin': 'http://localhost:8888',
     'Access-Control-Allow-Headers': 'Content-Type',
@@ -19,9 +139,7 @@ exports.handler = async (event) => {
     'Content-Type': 'application/json'
   };
 
-  // Handle preflight requests (HTTP OPTIONS method)
   if (event.httpMethod === 'OPTIONS') {
-    // Return a 200 status for preflight checks
     return {
       statusCode: 200,
       headers,
@@ -29,12 +147,37 @@ exports.handler = async (event) => {
     };
   }
 
-  // Handle POST requests
   if (event.httpMethod === 'POST') {
     try {
-      // Parse the request body
       const data = JSON.parse(event.body);
-      const { firstName, email, submittedAt } = data;
+      const { firstName, email } = data;
+
+      // Initialize the calendar
+      const calendar = new ICalCalendar({
+        name: 'Webinar Series'
+      });
+
+      const startDate = new Date('2024-04-26T09:00:00Z');
+      const endDate = new Date(startDate.getTime() + 1800000); // 30 minutes later
+
+      // Create event
+      calendar.createEvent({
+        start: startDate,
+        end: endDate,
+        summary: 'Webinar Registration',
+        description: 'Thank you for registering to our webinar series.',
+        organizer: {name: 'Jethro Block', email: 'jethro@thermalvisionresearch.co.uk'},
+        attendees: [{
+          email: email,
+          name: firstName,
+          rsvp: true,
+          role: 'REQ-PARTICIPANT',
+          status: 'ACCEPTED'
+        }]
+      });
+
+      // Generate the .ics file content
+      const icsString = calendar.toString();
 
       // Mail options for the user as a thank-you response
       const mailOptionsUser = {
@@ -46,10 +189,15 @@ exports.handler = async (event) => {
           <p>Thank you for registering to our webinar series. Your calendar invitation should be with you shortly.</p>
           <p>Kind Regards,<br>Jethro Block<br>Business Development Manager<br>07948 725 229<br>www.thermalvisionecology.co.uk<br>2530 The Quadrant, Aztec West, Bristol BS32 4AQ</p>
           <img src="https://i.ibb.co/092hq5D/tvrLOGO.png" alt="tvrLOGO" border="0" height="70px">
-        ` // Include your email signature as an image or HTML here
+        `,
+        attachments: [{
+          filename: 'event.ics',
+          content: icsString,
+          contentType: 'text/calendar'
+        }]
       };
 
-      // Send thank-you email to the user
+      // Send the email
       await new Promise((resolve, reject) => {
         transporter.sendMail(mailOptionsUser, (error, info) => {
           if (error) reject(error);
@@ -57,27 +205,13 @@ exports.handler = async (event) => {
         });
       });
 
-
- // Additional POST request to Zapier
- const zapierResponse = await fetch('https://hooks.zapier.com/hooks/catch/18365503/37hs1dq/', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data)
-});
-
-if (!zapierResponse.ok) throw new Error('Failed to send data to Zapier');
-
-
-      // Return success response with CORS headers
+      // Additional POST request to Zapier or handling other responses
       return {
         statusCode: 200,
         headers,
-        body: JSON.stringify({ message: 'Added row to spreadsheet' }),
+        body: JSON.stringify({ message: 'Added row to spreadsheet and sent email' }),
       };
     } catch (error) {
-      // Catch any errors and return a 500 error response with CORS headers
       return {
         statusCode: 500,
         headers,
@@ -86,7 +220,6 @@ if (!zapierResponse.ok) throw new Error('Failed to send data to Zapier');
     }
   }
 
-  // Respond to any other HTTP methods with a Method Not Allowed status
   return {
     statusCode: 405,
     headers,
