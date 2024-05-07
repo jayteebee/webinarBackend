46baa1a7e012895217c350b1afa857fa6aa5e9c0
Jethro Block <jethroblock22@gmail.com>
2024-05-07 11:57:39 +0100

    Update webinar registration email content

--- functions/add-row.js
+++ functions/add-row.js
@@ -40,7 +40,8 @@ exports.handler = async (event) => {
         subject: 'Thank you for registering to our Webinar Series',
         html: `
           <p>Hello ${firstName},</p>
-          <p>Thank you for registering to our webinar series. Your calendar invitation should be with you shortly.</p>
+          <p>Thank you for registering to the first webinar in our series, An Introduction to Thermal Imaging for R&D and Science, 29th May - 11am to 12pm. </p>
+          <p> Your calendar invitation should be with you shortly. If it hasn't arrived after a few minutes, please check your junk/spam folder.</p>
           <img src="https://i.ibb.co/zVbV7PM/Webinar-Thank-You-Flyer-2.png" alt="Webinar-Thank-You-Flyer-2" border="0">
           <p>Kind Regards,<br>Jethro Block<br>Business Development Manager<br>07948 725 229<br>www.thermalvisionecology.co.uk<br>2530 The Quadrant, Aztec West, Bristol BS32 4AQ</p>
           <img src="https://i.ibb.co/092hq5D/tvrLOGO.png" alt="tvrLOGO" border="0" height="70px">
